4be53fe0084be7241193ed39c729b7b88d432da6
tasn-i <raad979899@gmail.com>
2024-10-20 23:56:06 +0600

    like img added

--- js/Peddy.js
+++ js/Peddy.js
@@ -19,7 +19,7 @@ const removeActiveClass = () => {
     }
 }
 
-const loadButton = (id) => {
+const loadButton = (id, category) => {
     // alert(id);
     fetch(`https://openapi.programming-hero.com/api/peddy/category/${(id)}`)
     .then((res) => res.json())
@@ -33,13 +33,14 @@ const loadButton = (id) => {
         // activeBtn.classList.add("color")
         console.log(activeBtn);
         displaypets(data.data);
+        spacificCategory(category);
     })
     .catch((error) => console.log(error));
 
-    setTimeout (() =>{
-        loader.classList.add("hidden");
-        petsCards(data.data);
-    }, 2000)
+    // setTimeout (() =>{
+    //     loader.classList.add("hidden");
+    //     petsCards(data.data);
+    // }, 2000)
 }
 
 // Creates displayCatagories
@@ -175,7 +176,7 @@ pets. forEach ((pet) => {
          <p class="text-sm">Price: ${pet.price}</p>
       </div>
       <div class=" flex border rounded-md mt-2 p-3 cursor-pointer">
-         <img class="border rounded p-1" src="./new-img/right.png" alt="">
+         <img onclick ="liked('${pet.image}')" class="border rounded p-1" src="./new-img/right.png" alt="">
          <p id="disable" onclick ="counting()" class="text-sm border rounded p-1 mx-2 text-emerald-700">Adopt</p>
          <p onclick="loadDetails('${pet.petId}')" class="text-sm border rounded p-1 text-emerald-700">Details</p>
       </div>
@@ -207,30 +208,46 @@ const counting = () => {
     }, 1000);
   };
 
-// const loadButton = async (category) => {
-//     try {
+const spacificCategory = async (anyid) => {
+  const res = await fetch(`https://openapi.programming-hero.com/api/peddy/category/${anyid}`);
+  const data = await res.json();
+  const parentshow = document.getElementById("parentshow");
 
-//         const loader = document.getElementById('loader');
-//         loader.classList.remove("hidden");
-//         const response = await fetch(`https://openapi.programming-hero.com/api/peddy/category/${category}`);
-//         const data = await response.json();
+  const totalTime = 2000;
+  const interval = 1000;
 
+  let slice = totalTime / interval;
 
-//         setTimeout(() => {
-//             loader.classList.add("hidden"); // 
-//             showAllPets(data.data);
-//         }, 2000);
-//     } catch (error) {
-//         console.error('Error:', error);
+  const loadingpage = setInterval(function (){
+    parentshow.classList.remove("grid");
+    parentshow.innerHTML = `
+      <div class="flex justify-center items-center h-full">
+          <span class="loading loading-bars loading-lg"></span>
+      </div>
+    `
+    slice = slice - 1;
+  }, interval);
 
-//     }
-// };
+  setTimeout(function (){
+    clearInterval(intvId); 
+    displaypets(data.data);
+  }, totalTime);
+  
 
-// setTimeout(() => {
-//     loader.classList.add("hidden");
-//     // parent.classList.remove("allPets");
-//     showAllPets(data.data);
-// }, 2000);
+}
+
+// Like img show
+
+const liked = (image) => {
+  const showImageContainer = document.getElementById("showImg");
+  console.log(showImageContainer);
+  const showImage = document.createElement("div");
+  showImage.innerHTML =`
+     <img class="rounded-xl h-[120px] m-2" src = "${image}" alt="">
+  `
+  showImageContainer.appendChild(showImage);
+  
+}
 
 petsCards()
 
